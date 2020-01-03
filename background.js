e57579e68cb1ffa5963a0bdbd7c3c193254b35d6
JacksonKuo <JacksonKuo@users.noreply.github.com>
2020-01-03 04:35:01 -0600

    Added counter and color-changing icons

--- background.js
+++ background.js
@@ -1,9 +1,17 @@
+let count = 0;
+
 chrome.webRequest.onHeadersReceived.addListener(
 	function(details) { 
-		var headers = details["responseHeaders"];
+		var headers = details.responseHeaders;
 		for (x in headers) {
 			if (headers[x].name.includes("Content-Type") && headers[x].value.includes("application/x-java-serialized-object")) {
-				console.log("object found"); 
+			count++;
+			console.log("XJSO Check: " + details.url); 
+			if ( count == 1 ) {
+				chrome.browserAction.setIcon( { path: { 16: "shield-16-red.png" } } );
+			}
+			chrome.browserAction.setBadgeBackgroundColor( { color: [128, 128, 128, 255] } );
+			chrome.browserAction.setBadgeText( { text: count.toString() } );
 			}
 		}
 	},
